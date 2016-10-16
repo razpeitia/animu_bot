36a1d321ebbbc689cbebe594a0e64954ae80280d
Ricardo Azpeitia Pimentel <razpeitia@gmail.com>
2016-10-16 18:30:01 -0500

    Chaning url dispatch

--- src/animu_bot_dispatcher.erl
+++ src/animu_bot_dispatcher.erl
@@ -65,7 +65,7 @@ response(_ParsedRequest, Text) ->
 dispatch('GET', [<<"bot">>], _Req) ->
     {ok, [], <<"Animu bot v1.0">>};
 
-dispatch(Method, [<<"bot">>, ?BOT_TOKEN], Req)
+dispatch(Method, [<<"bot">>, BotToken], Req)
     when Method == 'GET'; Method == 'POST' ->
     lager:info("Dispatching..."),
     Headers = [{<<"content-type">>, <<"application/json">>}],
